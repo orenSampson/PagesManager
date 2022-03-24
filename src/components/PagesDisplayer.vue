cd3ad5fac0dc2b45e587e5b9ec8a569e71b44d89
Oren Sampson <oren.sampson@travelfactory.fr>
2022-03-24 13:01:41 +0200

    basic setup

---
+++ src/components/PagesDisplayer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>Pages Component</div>
+    <ul>
+        <li v-for="page in pages" :key="page.url">{{ page.url }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "PagesDisplayer",
+    props: {
+        pages: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style>
+</style>
--- src/components/PagesManager.vue
+++ src/components/PagesManager.vue
@@ -1,10 +1,17 @@
 <template>
     <div>PagesManager Component</div>
+    <pages-displayer :pages="pages" />
+    <button @click="addPage">Add Page</button>
 </template>
 
 <script>
+import PagesDisplayer from "./PagesDisplayer.vue";
+
 export default {
     name: "PagesManager",
+    components: {
+        PagesDisplayer,
+    },
     data() {
         return {
             pages: [
@@ -13,8 +20,27 @@ export default {
                     locale: "FR",
                     template: "blog",
                 },
+                {
+                    url: "cd",
+                    locale: "FR",
+                    template: "blog",
+                },
+                {
+                    url: "ef",
+                    locale: "FR",
+                    template: "blog",
+                },
             ],
         };
+    },
+    methods: {
+        addPage() {
+            this.pages.push({
+                url: "",
+                locale: "FR",
+                template: "blog",
+            });
+        },
     },
 };
 </script>
