a8476c210ed22c95699084d93207194e58ed3f0d
Oren Sampson <oren.sampson@travelfactory.fr>
2022-04-10 11:17:39 +0300

    popup click outside feature added

--- src/components/NewPageFormPopup.vue
+++ src/components/NewPageFormPopup.vue
@@ -1,80 +1,85 @@
 <template>
     <div class="popup">
-        <div class="popup-inner jumbotron jumbotron-style">
-            <div class="row">
-                <div class="col-2"></div>
-                <h3 class="col-8 text-center"><u>Add A New Page</u></h3>
-                <div class="col-1"></div>
-                <button
-                    class="btn btn-sm btn-success col-1 text-center"
-                    type="button"
-                    @click="onCancel"
-                >
-                    <strong>X</strong>
-                </button>
-            </div>
-
-            <form role="form" @submit.prevent="onSubmit">
-                <div
-                    class="form-group"
-                    :class="{ urlInvalid: !!this.urlErrorMessage }"
-                >
-                    <label for="url">URL</label>
-                    <input
-                        id="url"
-                        type="text"
-                        v-model.trim="url"
-                        @blur="checkIfURLValid"
-                        autocomplete="none"
-                        placeholder="Please Enter a vaild URL"
-                        class="form-control"
-                    />
-                    <p v-if="this.urlErrorMessage">
-                        * {{ this.urlErrorMessage }}
-                    </p>
-                </div>
-
-                <div>
-                    <label for="locale">Locale</label>
-                    <select
-                        id="locale"
-                        name="locale"
-                        v-model="locale"
-                        class="form-control m-b"
-                    >
-                        <option v-for="locale in localeOptions" :key="locale">
-                            {{ locale }}
-                        </option>
-                    </select>
-                </div>
-
-                <div>
-                    <label for="template">Template</label>
-                    <select
-                        id="template"
-                        name="template"
-                        v-model="template"
-                        class="form-control m-b"
-                    >
-                        <option
-                            v-for="template in templateOptions"
-                            :key="template"
-                        >
-                            {{ template }}
-                        </option>
-                    </select>
-                </div>
-                <div class="d-flex justify-content-center">
+        <div v-click-outside="onCancel">
+            <div class="popup-inner jumbotron jumbotron-style">
+                <div class="row">
+                    <div class="col-2"></div>
+                    <h3 class="col-8 text-center"><u>Add A New Page</u></h3>
+                    <div class="col-1"></div>
                     <button
-                        class="btn btn-success"
-                        type="submit"
-                        @click="onSubmit"
-                        :disabled="isSubmitBtnIsDisabled"
+                        class="btn btn-sm btn-success col-1 text-center"
+                        type="button"
+                        @click="onCancel"
                     >
-                        <strong>Create</strong>
+                        <strong>X</strong>
                     </button>
                 </div>
-            </form>
+
+                <form role="form" @submit.prevent="onSubmit">
+                    <div
+                        class="form-group"
+                        :class="{ urlInvalid: !!this.urlErrorMessage }"
+                    >
+                        <label for="url">URL</label>
+                        <input
+                            id="url"
+                            type="text"
+                            v-model.trim="url"
+                            @blur="checkIfURLValid"
+                            autocomplete="none"
+                            placeholder="Please Enter a vaild URL"
+                            class="form-control"
+                        />
+                        <p v-if="this.urlErrorMessage">
+                            * {{ this.urlErrorMessage }}
+                        </p>
+                    </div>
+
+                    <div>
+                        <label for="locale">Locale</label>
+                        <select
+                            id="locale"
+                            name="locale"
+                            v-model="locale"
+                            class="form-control m-b"
+                        >
+                            <option
+                                v-for="locale in localeOptions"
+                                :key="locale"
+                            >
+                                {{ locale }}
+                            </option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label for="template">Template</label>
+                        <select
+                            id="template"
+                            name="template"
+                            v-model="template"
+                            class="form-control m-b"
+                        >
+                            <option
+                                v-for="template in templateOptions"
+                                :key="template"
+                            >
+                                {{ template }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <button
+                            class="btn btn-success"
+                            type="submit"
+                            @click="onSubmit"
+                            :disabled="isSubmitBtnIsDisabled"
+                        >
+                            <strong>Create</strong>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </template>
@@ -92,6 +97,22 @@ export default {
             locale: LOCALE_OPTIONS?.length ? LOCALE_OPTIONS[0] : "",
             templateOptions: TEMPLATE_OPTIONS,
             template: TEMPLATE_OPTIONS?.length ? TEMPLATE_OPTIONS[0] : "",
+
+            vcoConfig: {
+                //data for click-outside-vue3 library
+                handler: this.handler,
+                middleware: this.middleware,
+                events: ["dblclick", "click"],
+                // Note: The default value is true, but in case you want to activate / deactivate
+                //       this directive dynamically use this attribute.
+                isActive: true,
+                // Note: The default value is true. See "Detecting Iframe Clicks" section
+                //       to understand why this behaviour is behind a flag.
+                detectIFrame: true,
+                // Note: The default value is false. Sets the capture option for EventTarget addEventListener method.
+                //       Could be useful if some event's handler calls stopPropagation method preventing event bubbling.
+                capture: false,
+            },
         };
     },
     methods: {
